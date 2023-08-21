65f4ff398b78b761114275c3b3465a7c83672182
Ritik Panchal <106887508+Ritikpcl@users.noreply.github.com>
2023-08-21 12:51:22 +0530

    Time: 20 ms (60.6%), Space: 15.1 MB (39.78%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,14 +1,44 @@
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         int n = s.length();
+
+//         for(int i=1; i<=n/2; i++){
+//             string temp = "";
+//             if(n%i == 0){
+//                 for(int j=0; j<n/i; j++) temp += s.substr(0,i);
+//             }
+//             if(temp == s) return true;
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
+        
         int n = s.length();
+        string temp = s + s;
 
-        for(int i=1; i<=n/2; i++){
-            string temp = "";
-            if(n%i == 0){
-                for(int j=0; j<n/i; j++) temp += s.substr(0,i);
-            }
-            if(temp == s) return true;
+        // deleting first and last characters
+        temp.erase(0,1);
+        temp.erase(2*n-2,1);
+        
+        vector<int> v(n);
+        int i=0, j=1, len = temp.length();
+        while(j<n){
+            if(s[i] == s[j]) v[j++] = ++i;
+            else if(i) i = v[i-1];
+            else j++; 
+        }
+
+        i=0,j=0;
+        while(j<len){
+            if(s[i] == temp[j]) i++,j++;
+            else if(i) i = v[i-1];
+            else j++;
+            if(i == n) return true;
         }
         return false;
     }
