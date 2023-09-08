95eea56cf0bb9332b5fb31f12b816380183a1adb
Ritik Panchal <106887508+Ritikpcl@users.noreply.github.com>
2023-09-08 23:21:57 +0530

    Time: 696 ms (5.01%), Space: 20.9 MB (43.81%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+        int m = matrix.size(), n = matrix[0].size(), sum = 0, ans = 0;
+        vector<vector<int>> dp(m+1, vector<int> (n+1,0));
+
+        for(int i=1; i<=m; i++){
+            sum = 0;
+            for(int j=1; j<=n; j++){
+                sum += matrix[i-1][j-1] == '1';
+                dp[i][j] = sum;
+            }
+        }
+
+        for(int i=1; i<=n; i++){
+            sum = 0;
+            for(int j=1; j<=m; j++){
+                sum += dp[j][i];
+                dp[j][i] = sum;
+            }
+        }
+
+        for(int k=1; k<=min(m,n); k++){
+            for(int i=k; i<=m; i++){
+                for(int j=k; j<=n; j++){
+                    int x = dp[i][j] - ( dp[i-k][j] + dp[i][j-k] - dp[i-k][j-k] );
+                    if(k*k == x){
+                        ans = x;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
